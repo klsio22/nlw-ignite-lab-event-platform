6363a967726b608f4a20de873fd44a21010cf875
Klesio Nascimento <klesio.dev@gmail.com>
2022-06-27 03:08:04 -0300

    finished sidebar structure

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,6 @@
 import { CheckCircle } from 'phosphor-react';
-
+import { isPast, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 interface LessonProps {
   title: string;
   slug: string;
@@ -8,23 +9,36 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableAtDateFormat = format(
+    props.availableAt,
+    "EEEE' • ' d 'de 'MMMM ' • ' k'h'mm",
+    {
+      locale: ptBR,
+    }
+  );
   return (
     <a href='#'>
-      <span className='text-gray-300'>
-        Segunda • 21 de junho • 19h00
-      </span>
+      <span className='text-gray-300'>{availableAtDateFormat}</span>
       <div className='rounded  border border-gray-500 p-4 mt-2 '>
         <header className='flex items-center justify-between'>
-          <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
-            <CheckCircle size={20} />
-            Conteúdo liberado
-          </span>
+          {isLessonAvailable ? (
+            <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className='text-sm text-orange-500 font-medium flex items-center gap-2'>
+              <CheckCircle size={20} />
+              Em breve
+            </span>
+          )}
 
           <span
             className='text-xs rounded px-2 py-[0.125rem] text-white border
            border-green-300 font-bold'
           >
-            AO VIVO
+            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
 
