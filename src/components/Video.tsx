efd836dfa337a817a2ece18a0be474eb281e8ab0
klesio <klesio.dev@gmail.com>
2022-12-20 18:27:48 -0300

    use codegen graphql to generation queries and mutations

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -9,33 +9,6 @@ import {
 
 import '@vime/core/themes/default.css';
 
-const GET_LESSONS_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      title
-      videoId
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`;
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    };
-  };
-}
 
 interface VideoProps {
   lessonSlug: string;
